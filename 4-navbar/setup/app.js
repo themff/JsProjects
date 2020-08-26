c82344d99b5c50afe4f4d5dd3455fa229e6c7bc4
Fk <themff@gmail.com>
2020-08-26 19:50:27 -0300

    working on branch 4

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector(".nav-toggle"); //search the nav-toggle button en el html y me lo traigo
+const links = document.querySelector(".links"); //me traigo la clase links
+
+navToggle.addEventListener("click", function(){
+    //console.log(links.classList.contains("links"));
+  /*  if(links.classList.contains("show-links")){
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add("show-links"); 
+    } */
+
+    links.classList.toggle("show-links");
+}) 
